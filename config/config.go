b65d2fd65fff92235ee249d1e6f8eea3a9dc6a31
OlegDjur <olegdjur@gmail.com>
2023-03-02 13:40:50 +0600

    added functions LoadConfig and ParsConfig

--- config/config.go
+++ config/config.go
@@ -1,10 +1,64 @@
 package config
 
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// App config struct
 type Config struct {
 	Server   ServerConfig
 	Postgres PostgresConfig
 }
 
-type ServerConfig struct{}
+// Server config struct
+type ServerConfig struct {
+	Port         string
+	JwtSecretKey string
+	CookieName   string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
 
-type PostgresConfig struct{}
+// Postgres config strcut
+type PostgresConfig struct {
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	Ssl              string
+}
+
+// Load config file from given path
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return v, nil
+}
+
+// Padse config file
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to dacode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
