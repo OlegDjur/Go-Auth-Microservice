51dd55bd76976e3a30f40c2ee15a509aebb6417b
oleg <olegdjur@gmail.com>
2023-03-07 20:09:37 +0600

    created function CheckEmail

--- internal/user/service/service.go
+++ internal/user/service/service.go
@@ -1,7 +1,23 @@
 package service
 
-import "github.com/OlegDjur/Go-Auth-Microservice/internal/user"
+import (
+	"github.com/OlegDjur/Go-Auth-Microservice/internal/models"
+	"github.com/OlegDjur/Go-Auth-Microservice/internal/user"
+	"github.com/OlegDjur/Go-Auth-Microservice/internal/user/dto"
+)
 
 type service struct {
-	repo user.Repository
+	repo *user.Repository
+}
+
+func NewService(repo *user.Repository) *service {
+	return &service{repo}
+}
+
+func (s *service) Create(user *dto.CreateUserRequest) (*models.User, error) {
+
+	user, err := s.repo.Create(user)
+	if err != nil {
+		return nil, err
+	}
 }
--- pkg/utils/validator.go
+++ pkg/utils/validator.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const alphaNumeric = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+const onlyAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func CheckEmail(email string) error {
+	emailRegex := regexp.MustCompile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+	if !emailRegex.MatchString(email) {
+		return fmt.Errorf("email: %v is not correct", email)
+	}
+
+	return nil
+}
